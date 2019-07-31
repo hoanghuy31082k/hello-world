4aac7f87003fca8ccf7e4a13301c30d5bd175d48
hoanghuy31082k <bunhuynh3108@gmail.com>
2019-07-31 23:55:37 +0700

    Report 31-7

--- asset/js/dauphongdauphay.js
+++ asset/js/dauphongdauphay.js
@@ -26,30 +26,57 @@ function xoauser(id) {
 		}
 	});
 }
-
 function datalist() {
-	console.log(1);
 	$.ajax ({
 		url : "index.php?controller=pages&action=getdatalist",
 		type : "get",
 		success : function (response) {
 			const result = $.parseJSON(response);
 			for (var i = 0;i <= result.length - 1; i++) {
-				var createtd_id = document.createElement("td");
-				var createtd_hoten = document.createElement("td");
-				var createtd_tuoi = document.createElement("td");
-				createtd_id.innerHTML= result[i].id;
-				createtd_hoten.innerHTML= result[i].hoten;
-				createtd_tuoi.innerHTML= result[i].tuoi;
-				$(`#list`).append(`<tr id="row_${result[i].id}"></tr>`);
-				$(`#row_${result[i].id}`).append(createtd_id);
-				$(`#row_${result[i].id}`).append(createtd_hoten);
-				$(`#row_${result[i].id}`).append(createtd_tuoi);
+				// var createtd_id = document.createElement("td");
+				// var createtd_hoten = document.createElement("td");
+				// var createtd_tuoi = document.createElement("td");
+				// createtd_id.innerHTML= result[i].id;
+				// createtd_hoten.innerHTML= result[i].hoten;
+				// createtd_tuoi.innerHTML= result[i].tuoi;
+				// $(`#list`).append(`<tr id="row_${result[i].id}"></tr>`);
+				// $(`#row_${result[i].id}`).append(createtd_id);
+				// $(`#row_${result[i].id}`).append(createtd_hoten);
+				// $(`#row_${result[i].id}`).append(createtd_tuoi);
+				$(`#list`).append(`<tr id="row_${result[i].id}">
+					<td>${result[i].id}</td>
+					<td>${result[i].hoten}</td>
+					<td>${result[i].tuoi}</td>
+					<td>
+						<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal${result[i].id}" style="width: 90px; height: 30px ; font-size: 12px; float: center;">Xoá bảng</button>
+					</td>
+					<td>
+						<a href="?controller=pages&action=update&id=${result[i].id}">Chỉnh sửa</a>
+					</td>
+				</tr>`);
+				$(`#list`).append(`<div id="myModal${result[i].id}" class="modal fade" role="dialog">
+						  <div class="modal-dialog">
+						    <div class="modal-content">
+						      <div class="modal-header">
+						        <button type="button" class="close" data-dismiss="modal">&times;</button>
+						        <h4 class="modal-title">Yêu cầu nho nhỏ :))</h4>
+						      </div>
+						      <div class="modal-body">
+						        <div class="alert alert-warning" id="result-delete-${result[i].id}">Bạn có muốn xoá database hay không?</div>
+						      </div>
+						      <div class="modal-footer">
+						         <button id="btn-delete-${result[i].id}" type="button" onclick="xoauser(${result[i].id});" class="btn btn-default" style="color: green">Có</button>
+						        <button id="btn-cancel-${result[i].id}" type="button" class="btn btn-default" data-dismiss="modal">Không</button>
+						      </div>
+						    </div>
+						  </div>
+				</div>`)
 			}
 		}
 	});
 }
 
+$(window).on("load" , datalist )
 function alo() {
 	alert('1');
 }
--- views/pages/home.php
+++ views/pages/home.php
@@ -7,41 +7,7 @@
 			<th>Tuổi</th>
 		</tr>
 	</thead>
-	<tbody id="list">
-		<!-- <tr>
-			<?php foreach ($pages as $row): ?>
-				<tr id="row_<?php echo $row->id; ?>">
-					<td><?php echo $row->id; ?></td>
-					<td><?php echo $row->hoten; ?></td>
-					<td><?php echo $row->tuoi; ?></td>
-					<td>
-						<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal<?php echo $row->id; ?>" style="width: 90px; height: 30px ; font-size: 12px; float: center;">Xoá bảng</button>
-					</td>
-					<td>
-						<a href="?controller=pages&action=update&id=<?php echo $row->id; ?>">Chỉnh sửa</a>
-					</td>
-					<div id="myModal<?php echo $row->id; ?>" class="modal fade" role="dialog">
-						  <div class="modal-dialog">
-
-						    <div class="modal-content">
-						      <div class="modal-header">
-						        <button type="button" class="close" data-dismiss="modal">&times;</button>
-						        <h4 class="modal-title">Yêu cầu nho nhỏ :))</h4>
-						      </div>
-						      <div class="modal-body">
-						        <div class="alert alert-warning" id="result-delete-<?php echo $row->id ?>">Bạn có muốn xoá database hay không?</div>
-						      </div>
-						      <div class="modal-footer">
-						         <button id="btn-delete-<?php echo $row->id ?>" type="button" onclick="xoauser(<?php echo $row->id ?>);" class="btn btn-default" style="color: green">Có</button>
-						        <button id="btn-cancel-<?php echo $row->id ?>" type="button" class="btn btn-default" data-dismiss="modal">Không</button>
-						      </div>
-						    </div>
-						  </div>
-						</div>
-				</tr>
-			<?php endforeach ?>
-		</tr> -->
-	</tbody>
+	<tbody id="list"></tbody>
 	<tfoot>
 		<form action="?controller=pages&action=them" method="POST">
 			<tr>
